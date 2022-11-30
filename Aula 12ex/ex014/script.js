8989c25bc23abd62b4dfcf270e29c81d2f1a754d
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2022-11-30 18:52:07 -0300

    Finalizando o Exercício 01 da aula 12

---
+++ Aula 12ex/ex014/script.js
@@ -0,0 +1,17 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()    
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/fotomanha.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/fototarde.jpg'
+        document.body.style.background = '#468eec'
+    } else {
+        img.src = 'imagens/fotonoite.jpg'
+        document.body.style.background = '#042857'
+    }
+}
