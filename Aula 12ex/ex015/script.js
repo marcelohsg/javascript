f98677dff3ca22354ddbc7bcf2a57d8155ee1546
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2022-12-01 11:01:58 -0300

    Finalizando o ex015

---
+++ Aula 12ex/ex015/script.js
@@ -0,0 +1,49 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = window.document.getElementById('res')
+    if (fano.value.lenght == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'imagens/foto-bebe-m.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'imagens/foto-jovem-m.png')
+            } else if (idade < 60) {
+                //adulto
+                img.setAttribute('src', 'imagens/foto-adulto-m.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagens/foto-idoso-m.png')
+            }
+        } else {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'imagens/foto-bebe-f.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'imagens/foto-jovem-f.png')
+            } else if (idade < 60) {
+                //adulto
+                img.setAttribute('src', 'imagens/foto-adulto-f.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagens/foto-idoso-f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `<p>Detectamos: ${gênero} com ${idade} anos.</p>`
+        res.appendChild(img)
+    }
+}
