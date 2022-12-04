ba37cae6c73f8f07e0977acef25203ea7cfc7741
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2022-12-04 19:28:05 -0300

    Create for.js

---
+++ Aula 14 - Repetição 02/for.js
@@ -0,0 +1,13 @@
+/*
+var c=1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+console.log('Vai começar...')
+for (var c=1;c<=10;c++) {
+    console.log (`Passo ${c}`)
+}
+console.log('FIM!')
